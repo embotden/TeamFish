f52dded27e4ed2f5a388dd259ef92d667163f17e
Em Botden <mm.botden@gmail.com>
2022-09-16 17:42:36 +0200

    player climbing script (seperate)

--- TeamFishVrij/Assets/Scripts/Player/PlayerClimbing.cs
+++ TeamFishVrij/Assets/Scripts/Player/PlayerClimbing.cs
@@ -2,8 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Implementing climbing - HamJoy Games
+
 public class PlayerClimbing : MonoBehaviour
 {
+
+    [Header("References")]
+    public Transform orientation;
+    public Rigidbody _rb;
+    public LayerMask whatIsWall;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,29 @@ public class PlayerClimbing : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //DOWNWARD CAST TO FIND A LEDGE
+
+
+        //FORWARD CAST TO SAMPLE THE VERTICAL SURFACE
+
+
+        //OVERPASS CAST TO SEE IF WE SHOULD JUST WALK UNDERNEATH IT
+
+
+        //DETERMINE IF LEDGE IS NOT TOO STEEP
+
+
+        //CHECK HOW DIRECTLY THE CHARACTER IS MOVING TOWARDS THE WALL
+
+
+        //APPLY AN OFFSET TO MOVE AWAY FROM THE EDGE
+
+
+        //DEPENETRATE THE POINT FOR THE CAPSULE
+
+
+        //CHECK FOR SPACE BY DOING A CAPSULE SWEEP UP AND FORWARD
     }
+
+
 }
