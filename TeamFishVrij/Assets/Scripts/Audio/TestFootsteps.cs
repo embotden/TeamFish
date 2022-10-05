71971855268bfdc1428e33db0c328ecd77b61cfa
Lelah <lvaneijnsbergen@outlook.com>
2022-10-05 17:18:37 +0200

    Audio tests in various test scenes

---
+++ TeamFishVrij/Assets/Scripts/Audio/TestFootsteps.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestFootsteps : MonoBehaviour {
+
+    [FMODUnity.EventRef]
+    public string EventReference;
+    public string inputsound;
+    bool playerismoving;
+    public float walkingspeed;
+
+}
+
