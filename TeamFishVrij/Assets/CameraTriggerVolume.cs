8ba1a6ddbafbd79c061a29585d83894a704ae5da
Em Botden <mm.botden@gmail.com>
2022-10-19 12:36:18 +0200

    look out point + camera bug fix

---
+++ TeamFishVrij/Assets/CameraTriggerVolume.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+
+public class CameraTriggerVolume : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+
+    private bool _playerCamera = true;
+
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Fhinn")) SwitchState();
+        Debug.Log("Look out point");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Fhinn")) SwitchState();
+        Debug.Log("player camera");
+    }
+
+    private void SwitchState()
+    {
+        if (_playerCamera)
+        {
+            _animator.Play("Lookout camera");
+        }
+        else
+        {
+            _animator.Play("Player camera");
+        }
+        _playerCamera = !_playerCamera;
+    }
+}
