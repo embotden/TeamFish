3b37ff475d9b0f3bd780a21323bd71efaeb78a8b
embotden <54795304+embotden@users.noreply.github.com>
2022-10-26 11:04:25 +0200

    main menu code test
    test voor het aanpassen van de code voor het hoofdmenu, maar deze versie werkt nog niet

--- TeamFishVrij/Assets/Scripts/Menu/MainMenuNavigator.cs
+++ TeamFishVrij/Assets/Scripts/Menu/MainMenuNavigator.cs
@@ -11,6 +11,8 @@ public class MainMenuNavigator : MonoBehaviour
     public Animator _menuAnimator;
 
     public bool _isWatching { get; private set; }
+    [SerializeField] private bool _canExit;
+    [SerializeField] private bool _canSwitch;
 
     // Start is called before the first frame update
     void Awake()
@@ -18,14 +20,31 @@ public class MainMenuNavigator : MonoBehaviour
         _headerOption.SetActive(false);
         _visualCue.SetActive(false);
         _isWatching = false;
+        _canExit = true;
+        _canSwitch = false;
     }
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Escape))
+        if(_canSwitch && Input.GetKeyDown(KeyCode.Q))
         {
             SwitchState();
         }
+
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(_canExit)
+            {
+                //Turn on are you sure UI
+
+                Debug.Log("Suggesting quit game");
+            }
+            else
+            {
+                SwitchToMainState();
+            }
+        }
+
     }
 
     private void OnTriggerEnter(Collider other)
@@ -35,7 +54,7 @@ public class MainMenuNavigator : MonoBehaviour
             _headerOption.SetActive(true);
             _visualCue.SetActive(true);
 
-            if(Input.GetKeyDown(KeyCode.Q)) SwitchState();
+            _canSwitch = true;
         }
     }
 
@@ -45,28 +64,34 @@ public class MainMenuNavigator : MonoBehaviour
         {
             _headerOption.SetActive(false);
             _visualCue.SetActive(false);
+
+            _canSwitch = false;
         }
     }
 
     private void SwitchState()
     {
-        _isWatching = true;
-
+        //Debug.Log("Switching state...");
+                
         if (gameObject.CompareTag("Options"))
         {
             _menuAnimator.Play("Window camera");
+            _isWatching = true;
         }
         else if (gameObject.CompareTag("Story"))
         {
             _menuAnimator.Play("Exposition camera");
+            _isWatching = true;
         }
         else if (gameObject.CompareTag("Collection"))
         {
             _menuAnimator.Play("Memories camera");
+            _isWatching = true;
         }
         else if (gameObject.CompareTag("Bath"))
         {
             _menuAnimator.Play("Start camera");
+            _isWatching = true;
         }
         else
         {
@@ -77,7 +102,6 @@ public class MainMenuNavigator : MonoBehaviour
     private void SwitchToMainState()
     {
         _menuAnimator.Play("Overview camera");
-
         _isWatching = false;
     }
 }
