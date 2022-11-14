9e984aab6ac11292952bc823ffa6cba3c719cb15
embotden <54795304+embotden@users.noreply.github.com>
2022-11-14 15:41:41 +0100

    setup gargoyle puzzle

---
+++ TeamFishVrij/Assets/GargoyleManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GargoyleManager : MonoBehaviour
+{
+    [Header("Gargoyle state")]
+    public float _waterLevel;
+    [SerializeField] private float _waterImpact;
+    [SerializeField] private float _waterMinimum;
+
+}
